8b7bd2b53397a720dfeb57228ee8c649d04e00e3
aprilseo <seoa488@newschool.edu>
2024-02-16 04:41:33 -0500

    launch

---
+++ script1.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var showButton = document.getElementById('showButton');
+    var showButton2 = document.getElementById('showButton2');
+    var container2 = document.getElementById('container2');
+
+    showButton.addEventListener('click', function() {
+        container2.style.display = 'block';
+    });
+
+    showButton2.addEventListener('click', function() {
+    });
+});
